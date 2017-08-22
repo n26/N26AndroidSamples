566e5c060665ad728a43ee9b922f39e13e445b71
Lucia Payo <lucia.payo@n26.com>
2017-08-22 14:46:06 +0200

    Configure home activity to be shown as entry point in the app

--- app/src/main/java/de/n26/n26androidsamples/home/presentation/HomeActivity.java
+++ app/src/main/java/de/n26/n26androidsamples/home/presentation/HomeActivity.java
@@ -1,21 +1,31 @@
 package de.n26.n26androidsamples.home.presentation;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import de.n26.n26androidsamples.R;
 import de.n26.n26androidsamples.application.N26SamplesApplication;
 import de.n26.n26androidsamples.base.presentation.base.BaseInjectingActivity;
 import de.n26.n26androidsamples.base.presentation.injection.modules.ActivityModule;
+import polanski.option.Option;
 
 public class HomeActivity extends BaseInjectingActivity<HomeActivityComponent> {
 
     @Override
-    protected void onInject(@NonNull final HomeActivityComponent homeActivityComponent) {
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Option.ofObj(savedInstanceState).ifNone(this::showCreditFragment);
+    }
 
+    @Override
+    protected void onInject(@NonNull final HomeActivityComponent homeActivityComponent) {
+        homeActivityComponent.inject(this);
     }
 
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_home;
     }
 
     @NonNull
@@ -24,5 +34,8 @@ public class HomeActivity extends BaseInjectingActivity<HomeActivityComponent> {
         N26SamplesApplication app = N26SamplesApplication.class.cast(getApplication());
         ActivityModule activityModule = new ActivityModule();
         return app.getComponent().createHomeActivityComponent(activityModule);
+    }
+
+    private void showCreditFragment() {
     }
 }
